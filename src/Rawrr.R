7b744a5013bd4644eabf5939546627a7b679a4d2
Christian Panse <cp@fgcz.ethz.ch>
2021-12-03 16:21:03 +0100

    Adapt Linux settings

--- src/Rawrr.R
+++ src/Rawrr.R
@@ -1,13 +1,15 @@
 #R
+stopifnot( packageVersion('rawrr') > '1.3.1')
 
+Sys.setenv("MONO_LOG_LEVEL"="all")
+Sys.setenv("MONO_LOG_MASK"="dll,cfg")
 Sys.setenv("PKG_CPPFLAGS"="`pkg-config --cflags mono-2` -O3")
 Sys.setenv("PKG_CXXFLAGS"="`pkg-config --cflags mono-2` -O3")
 Sys.setenv("PKG_LIBS"="`pkg-config --libs mono-2`")
 
 
-Rcpp::sourceCpp("rawrrRcpp.cpp", cacheDir = "/tmp/cpanse/Rcpp3/", showOutput=TRUE, verbose=TRUE, rebuild = TRUE)
+Rcpp::sourceCpp("rawrrRcpp.cpp", cacheDir = "RcppMonoEmbed", showOutput=TRUE, verbose=TRUE, rebuild = TRUE)
 
-f<-"/home/cpanse/.cache/R/rawrr/20181113_010_autoQC01.raw"
 EH4547 <- "/home/cpanse/.cache/R/ExperimentHub/18e7d5bfb4a3b_4590.raw"
 
 R <- new(Rawrr)
@@ -15,11 +17,14 @@ R$setDomainName("rawrrRcpp")
 R$createObject()
 R$setRawFile(EH4547)
 R$openFile()
+mZ <- R$get_values(9594, "CentroidScan.Masses"); intensity <- R$get_values(9594, "CentroidScan.Intensities")
+head(mZ)
+head(intensity)
 
 
-set.seed(2)
 
-.readSpectrumRcpp <- function(f, scan){
+.readSpectrumRcpp <- function(f, scan, mode){
+  R$setRawFile(f)
   rv <- lapply(scan, function(i){
      list(
      	trailer = R$get_trailer(i),
@@ -27,32 +32,42 @@ set.seed(2)
         intensity = R$get_values(i, "CentroidScan.Intensities")
 	)
   })
- # R$dector()
-
 rv
 }
 
+bm <- lapply(2^(0:13), function(n, rawfile, ...){
+	message(n)
+         m0 <-  microbenchmark::microbenchmark({S0 <- rawrr::readSpectrum(rawfile, 1:n, mode='default')}, ...)
+         m1 <-  microbenchmark::microbenchmark({S1 <- rawrr::readSpectrum(rawfile, 1:n, mode='barebone')}, ...)
+         m2 <-  microbenchmark::microbenchmark({S2 <- .readSpectrumRcpp(rawfile, 1:n, mode='monoembed')}, ...)
 
+	 testthat::expect_setequal(
+         	 unlist( sapply(S0, function(x) x$mZ)),
+         	 unlist( sapply(S1, function(x) x$mZ)))
 
-lapply(1:10,function(y){
-b <- lapply(2^(0:14), function(x){
-  smp <- sample(2^14,x, replace=FALSE)
-  start_time <- Sys.time()
-S <- .readSpectrumRcpp(f, scan=smp) 
-  end_time <- Sys.time()
-  R$get_info()
-  message(paste(x, td <- end_time - start_time))
-  b1 <- data.frame(n=x, time=td, type='Rcpp')
+	 testthat::expect_setequal(
+         	 unlist( sapply(S1, function(x) x$mZ)),
+         	 unlist( sapply(S2, function(x) x$mZ)))
 
-  start_time <- Sys.time()
-  S<- rawrr::readSpectrum(EH4547, smp)
-  end_time <- Sys.time()
-  message(paste(x, td <- end_time - start_time))
-  b2 <- data.frame(n=x, time=td, type='system2')
-  rbind(b1,b2)
-})   |> Reduce(f=rbind)
+	 testthat::expect_setequal(
+         	 unlist( sapply(S0, function(x) x$intensity)),
+         	 unlist( sapply(S1, function(x) x$intensity)))
 
-write.table(b, file="../vignettes/benchmark2.txt", row.names = FALSE, append=TRUE)
-})
-quit("yes")
+	 testthat::expect_setequal(
+         	 unlist( sapply(S1, function(x) x$intensity)),
+         	 unlist( sapply(S2, function(x) x$intensity)))
+
+	 rv <- list(
+	 data.frame(time = m0$time, mode='default', n=n),
+	 data.frame(time = m1$time, mode='barebone', n=n),
+	 data.frame(time = m2$time, mode='monoembed', n=n))
+	 rv |> Reduce(f='rbind')
+
+  }, times = 2, unit = "nanosecond", rawfile = EH4547) |> Reduce(f='rbind')
+
+bm
+lattice::xyplot(time ~ n|mode, data=bm, scales=list(log=TRUE))
+lattice::xyplot(time ~ n, groups=bm$mode, data=bm,
+  type='p', scale=list(log=TRUE), ylab='time [in nanosecond]', xlab='number of spectra', auto.key=TRUE)
+quit('yes')
 
